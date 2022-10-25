fa08d3fd6cecced6c60c18d9a2b38755857dfd95
obador <obador@akamai.com>
2022-10-25 16:06:17 +0300

    xx

--- main.py
+++ main.py
@@ -1,38 +1,70 @@
 from tkinter import *
 
-root = Tk()
-root.title("unknown")
-root.geometry("500x500")
 
-w=500
-h=500
-x=w//2
-y=h//2
+def gui():
+    root = Tk()
+    root.title("unknown")
+    root.geometry("1000x1000")
+
+    w = 800
+    h = 600
+    x = w // 2
+    y = h // 2
+    i = 0
+    press = False
+    can = Canvas(root, width=w, height=h, bg='black')
+    can.pack()
+    # Create line________(x1, y1, x2, y2, fill="color")
+    can.create_line(0, 300, 800, 300, fill="red")
+    can.create_line(400, 0, 400, 800, fill="red")
+    my_circle = can.create_oval(x - 20, y - 20, x + 20, y + 20, outline='red', fill='blue')
+
+    def left(event):
+        x = -10
+        y = 0
+        can.move(my_circle, x, y)
+
+    def right(event):
+        x = +10
+        y = 0
+        can.move(my_circle, x, y)
+
+    def up(event):
+        x = 0
+        y = -10
+        can.move(my_circle, x, y)
+
+    def down(event):
+        x = 0
+        y = +10
+        can.move(my_circle, x, y)
+
+    def button():
+        global i
+        if i % 2 == 0:
+            press = True
+        if i % 2 == 1:
+            press = False
+        i = i + 1
+
+    if press:
+        print("true")
 
 
-#create rectangle (root,  x           y             color)
-my_canvas = Canvas(root, width=500, height=500, bg="white")
+    button1 = Button(root, text="Move", bg='white', command=button)
+    button1.pack()
 
-my_canvas.pack(pady=0)
-# Create line________(x1, y1, x2, y2, fill="color")
-my_canvas.create_line(0, 0, 500, 500, fill="red")
+    root.bind("<Up>", up)
+    root.bind("<Right>", right)
+    root.bind("<Left>", left)
+    root.bind("<Down>", down)
 
-# Create rectangle________(x1, y1, x2, y2, fill="color")
-#Rectangle x1,y1: Top Left
-#Rectangle x2,y2: Bottom Right
-my_canvas.create_rectangle(50, 450, 450, 50, fill="blue")
-
-# Create Oval
-# Oval x1,y1:
-y = my_canvas.create_oval(60, 100, 110, 50, fill="yellow")
-
-
-root.mainloop()
-
+    root.mainloop()
 
 
 def main():
-    print("gg")
+    gui()
+    print("0")
 
 
 if __name__ == '__main__':
